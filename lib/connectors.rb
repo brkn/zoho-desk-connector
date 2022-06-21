b15d64d90d5c5c38ee129732fe28c945d6b0bb77
Berkan Unal <Berkanunal@gmail.com>
2022-06-21 13:47:52 +0300

    Init repo

---
+++ lib/connectors.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Connectors
+  require 'connectors/zoho_desk'
+end
---
+++ lib/connectors/zoho_desk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Connectors
+  class ZohoDeskError < StandardError; end
+
+  class ZohoDesk
+  end
+end
